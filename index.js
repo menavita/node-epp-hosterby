e8925fa1c917d50d8d0e4fc95e5e8fdb95bf017a
Egor Kuryanovich <me@sontan.name>
2015-02-02 09:23:10 +0300

    Теперь закрывает соединение после выполненной команды.

--- index.js
+++ index.js
@@ -12,50 +12,70 @@ function HosterBY (credential)
 
 HosterBY.prototype.checkDomain = function(domainName)
 {
-  var _this = this;
+  var self = this;
   var d = deferred();
   var state = new ProtocolState('hosterby', this.credential);
 
   state.connection.initStream().then(function() 
   {
-    state.login({ "login": _this.credential.login, "password": _this.credential.password }, ['login', moment().unix()].join('-'))
+    state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
     .then(
       function(result) 
       {
         state.command('checkDomain', { name: domainName }, ['checkDomain', moment().unix()].join('-') )
         .then(
-          function(result) { d.resolve(result.data['domain:chkData']['domain:cd']['domain:name']); },
-          function(error) { d.reject(error); });
+          function(result) 
+          { 
+            d.resolve(result.data['domain:chkData']['domain:cd']['domain:name']); 
+            state.command('logout', null, ['logout', moment().unix()].join('-')); 
+          },
+          function(error) 
+          { 
+            d.reject(error); 
+            state.command('logout', null, ['logout', moment().unix()].join('-')); 
+          });
       },
-      function(error) { d.reject(error); });
+      function(error) 
+      { 
+        d.reject(error); 
+        state.command('logout', null, ['logout', moment().unix()].join('-')); 
+      });
   });
-
-  setTimeout(function() { state.command('logout', null, ['logout', moment().unix()].join('-')); }, 5000);
 
   return d.promise;
 }
 
 HosterBY.prototype.createDomain = function(domainName)
 {
-  var _this = this;
+  var self = this;
   var d = deferred();
   var state = new ProtocolState('hosterby', this.credential);
 
   state.connection.initStream().then(function() 
   {
-    state.login({ "login": _this.credential.login, "password": _this.credential.password }, ['login', moment().unix()].join('-'))
+    state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
     .then(
       function(result) 
       {
         state.command('createDomain', { name: domainName, registrant: 4821, ns: { "domain:hostObj": ['ns1.staronka.by','ns2.staronka.by'] } }, ['createDomain', moment().unix()].join('-') )
         .then(
-          function(result) { d.resolve(result.data['domain:chkData']['domain:cd']['domain:name']); },
-          function(error) { d.reject(error); });
+          function(result) 
+          { 
+            d.resolve(result.data['domain:chkData']['domain:cd']['domain:name']);
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          },
+          function(error) 
+          { 
+            d.reject(error); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          });
       },
-      function(error) { d.reject(error); });
+      function(error) 
+      { 
+        d.reject(error); 
+        state.command('logout', null, ['logout', moment().unix()].join('-'));
+      });
   });
-
-  setTimeout(function() { state.command('logout', null, ['logout', moment().unix()].join('-')); }, 5000);
 
   return d.promise;
 }
@@ -64,7 +84,7 @@ HosterBY.prototype.createDomain = function(domainName)
 HosterBY.prototype.createContact = function(contactInfo)
 {
   console.log(contactInfo);
-  var _this = this;
+  var self = this;
   var d = deferred();
   var state = new ProtocolState('hosterby', this.credential);
 
@@ -82,19 +102,29 @@ HosterBY.prototype.createContact = function(contactInfo)
 
   state.connection.initStream().then(function() 
   {
-    state.login({ "login": _this.credential.login, "password": _this.credential.password }, ['login', moment().unix()].join('-'))
+    state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
     .then(
       function(result) 
       {
         state.command('createContact', contactInfoCompiled, ['createContact', moment().unix()].join('-') )
         .then(
-          function(result) { d.resolve(result.data); },
-          function(error) { d.reject(error); });
+          function(result) 
+          { 
+            d.resolve(result.data); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          },
+          function(error) 
+          { 
+            d.reject(error); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          });
       },
-      function(error) { d.reject(error); });
+      function(error) 
+      { 
+        d.reject(error);
+        state.command('logout', null, ['logout', moment().unix()].join('-'));
+      });
   });
-
-  setTimeout(function() { state.command('logout', null, ['logout', moment().unix()].join('-')); }, 5000);
 
   return d.promise;
 }
