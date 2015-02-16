589c6563cec364ef3c48770303f776982cd86826
Egor Kuryanovich <me@sontan.name>
2015-02-16 13:22:52 +0300

    Подготовка к загрузке в продакшен.

--- index.js
+++ index.js
@@ -45,33 +45,44 @@ HosterBY.prototype.checkDomain = function(domainName)
   return d.promise;
 }
 
-HosterBY.prototype.createDomain = function(domainName)
+HosterBY.prototype.createDomain = function(domainName, contactId, domainTerm)
 {
   var self = this;
   var d = deferred();
   var state = new ProtocolState('hosterby', this.credential);
 
+  var domainInfoCompiled = {
+    name: domainName, 
+    registrant: contactId, 
+    period: domainTerm, 
+    ns: { "domain:hostObj": ['ns1.staronka.by','ns2.staronka.by'] } 
+  };
+  
   state.connection.initStream().then(function() 
   {
     state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
     .then(
       function(result) 
       {
-        state.command('createDomain', { name: domainName, registrant: 4821, ns: { "domain:hostObj": ['ns1.staronka.by','ns2.staronka.by'] } }, ['createDomain', moment().unix()].join('-') )
+        //console.log(result);
+        state.execute('createDomain', domainInfoCompiled, ['createDomain', moment().unix()].join('-') )
         .then(
           function(result) 
           { 
-            d.resolve(result.data['domain:chkData']['domain:cd']['domain:name']);
+            //console.log('result');
+            d.resolve(result.data);
             state.command('logout', null, ['logout', moment().unix()].join('-'));
           },
           function(error) 
           { 
+            //console.log('error');
             d.reject(error); 
             state.command('logout', null, ['logout', moment().unix()].join('-'));
           });
       },
       function(error) 
       { 
+        //console.log('error2');
         d.reject(error); 
         state.command('logout', null, ['logout', moment().unix()].join('-'));
       });
@@ -83,7 +94,7 @@ HosterBY.prototype.createDomain = function(domainName)
 
 HosterBY.prototype.createContact = function(contactInfo)
 {
-  console.log(contactInfo);
+  //console.log(contactInfo);
   var self = this;
   var d = deferred();
   var state = new ProtocolState('hosterby', this.credential);
@@ -106,15 +117,119 @@ HosterBY.prototype.createContact = function(contactInfo)
     .then(
       function(result) 
       {
-        state.command('createContact', contactInfoCompiled, ['createContact', moment().unix()].join('-') )
+        state.execute('createContact', contactInfoCompiled, ['createContact', moment().unix()].join('-') )
         .then(
           function(result) 
           { 
-            d.resolve(result.data); 
+            //console.log('createContact.resolve');
+            d.resolve(result); 
             state.command('logout', null, ['logout', moment().unix()].join('-'));
           },
           function(error) 
           { 
+            //console.log('createContact.reject');
+            d.reject(error); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          });
+      },
+      function(error) 
+      { 
+        d.reject(error);
+        state.command('logout', null, ['logout', moment().unix()].join('-'));
+      });
+  });
+
+  return d.promise;
+}
+
+
+HosterBY.prototype.updateContact = function(contactId, contactInfo)
+{
+  var self = this;
+  var d = deferred();
+  var state = new ProtocolState('hosterby', this.credential);
+
+  var contactInfoCompiled = 
+  {
+    "update": 
+    {
+      "contact:update": {
+        "_attr": 
+        {
+            "xmlns:contact": this.credential.namespaces.contact.xmlns,
+            "xsi:schemaLocation": "urn:ietf:params:xml:ns:contact-1.0 contact-1.0.xsd"
+
+        },
+        "contact:id": contactId,
+        "contact:chg": contactInfo['contact']
+      }
+    },
+    "extension": { "by-ext-contact:update": contactInfo['extension'] }
+  };
+/*
+  contactInfoCompiled["create"]["contact:update"]["_attr"] = 
+  { 
+    "xmlns:contact": this.credential.namespaces.contact.xmlns,
+    "xsi:schemaLocation": "urn:ietf:params:xml:ns:contact-1.0 contact-1.0.xsd"
+  };
+  contactInfoCompiled["create"]["contact:create"]["contact:postalInfo"]["_attr"] = { "type": 'loc' };
+  contactInfoCompiled["extension"]["by-ext-contact:create"]["_attr"] = this.credential.namespaces.extension;
+*/
+  state.connection.initStream().then(function() 
+  {
+    state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
+    .then(
+      function(result) 
+      {
+        state.execute('updateContact', contactInfoCompiled, ['updateContact', moment().unix()].join('-') )
+        .then(
+          function(result) 
+          { 
+            //console.log('updateContact.resolve');
+            d.resolve(result); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          },
+          function(error) 
+          { 
+            //console.log('updateContact.reject');
+            d.reject(error); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          });
+      },
+      function(error) 
+      { 
+        d.reject(error);
+        state.command('logout', null, ['logout', moment().unix()].join('-'));
+      });
+  });
+
+  return d.promise;
+}
+
+
+HosterBY.prototype.infoContact = function(contactId)
+{
+  var self = this;
+  var d = deferred();
+  var state = new ProtocolState('hosterby', this.credential);
+
+  state.connection.initStream().then(function() 
+  {
+    state.login({ "login": self.credential.login, "password": self.credential.password }, ['login', moment().unix()].join('-'))
+    .then(
+      function(result) 
+      {
+        state.execute('infoContact', {id:contactId }, ['infoContact', moment().unix()].join('-') )
+        .then(
+          function(result) 
+          { 
+            //console.log('infoContact.resolve');
+            d.resolve(result); 
+            state.command('logout', null, ['logout', moment().unix()].join('-'));
+          },
+          function(error) 
+          { 
+            //console.log('infoContact.reject');
             d.reject(error); 
             state.command('logout', null, ['logout', moment().unix()].join('-'));
           });
